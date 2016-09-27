81fa3d186e2f9903016359ad1cba2a13357383f4
Soumen Bandyopadhyay <soumen28@users.noreply.github.com>
2016-09-27 16:36:39 -0700

    first commit for DF demo

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+var Promise = require("bluebird");
+
+var nforce = require('nforce');
+var org = require('../app');
+
+const util = require('util');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Salesforce Platform Events <-> Heroku Demo' });
+});
+
+/* Creates a new the record */
+router.post('/', function(req, res, next) {
+	console.log('In router post');
+	var newEvent = nforce.createSObject('Customer_Order__e');
+	newEvent.set('CustomerId__c', req.body.customerId);
+	newEvent.set('Product_Name__c', req.body.productName);
+	newEvent.set('Number_of_Units__c', req.body.numberOfUnits);
+	newEvent.set('Total_Amount__c', req.body.totalAmount);  
+
+	org.org.insert({ sobject: newEvent })
+  
+});
+
+module.exports = router;
