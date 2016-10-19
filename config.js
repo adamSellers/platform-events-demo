76bb17dc4ae8d20b7741fadcd0a37133c9e2aacf
Jay Hurst <jhurst@salesforce.com>
2016-10-19 13:34:07 -0700

    First Push

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.ENVIRONMENT = 'production';
 exports.CALLBACK_URL = 'http://localhost:3001';
 exports.TOPIC = '/event/Order_Response__e';
 
-exports.CLIENT_ID = "3MVG9SemV5D80oBcjsq97hHAUFcvbzU68h8Qd3DoYvB4fHUxGviPd57zmDEzTAFEsbaQUGyY1OOxJ_ACNMv5s";
-exports.CLIENT_SECRET = "8410631505093561599";
-exports.USERNAME = "admin@acmesales.com";
-exports.PASSWORD = "test1234";
+exports.CLIENT_ID = "";
+exports.CLIENT_SECRET = "";
+exports.USERNAME = "";
+exports.PASSWORD = "";
